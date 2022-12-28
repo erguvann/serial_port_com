f6e41280aa37566081a4a3093ce282676113ab0d
erguvann <cihansenyuz@hotmail.com>
2022-12-28 14:31:19 +0300

    object interactions added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_send_button_clicked()
+{
+    ui->chat_box->appendPlainText(ui->message_line->text());
+    ui->message_line->clear();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_send_button_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
